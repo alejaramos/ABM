dba343f781b33621eb0cabd62008797bc484c0de
Alejandra Ramos <101682507+alejaramos@users.noreply.github.com>
2022-07-05 11:37:38 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 import {Login} from '../Components/Login'
 
 export default function Home() {
